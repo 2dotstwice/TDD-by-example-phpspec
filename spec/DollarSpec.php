ed3fbe1af952dcc22fd0dd6172d53f92b8579f79
Kristof Coomans <kristof@coomans.eu>
2015-04-12 11:19:04 +0200

    Chapter 1: add test for multiplication

--- spec/DollarSpec.php
+++ spec/DollarSpec.php
@@ -4,11 +4,17 @@ namespace spec\WyCash\WyCash;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use WyCash\WyCash\Dollar;
 
+/**
+ * @mixin Dollar
+ */
 class DollarSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    function it_multiplies()
     {
-        $this->shouldHaveType('WyCash\WyCash\Dollar');
+        $this->beConstructedWith(5);
+        $this->times(2);
+        $this->amount->shouldBe(10);
     }
 }
