398fdc71bb467eaaed0acf8e3241764a54e6e775
Kristof Coomans <kristof@coomans.eu>
2015-04-12 11:22:14 +0200

    Chapter 1: Add Dollar constructor and times() generated by phpspec

--- src/Dollar.php
+++ src/Dollar.php
@@ -4,4 +4,14 @@ namespace WyCash\WyCash;
 
 class Dollar
 {
+
+    public function __construct($argument1)
+    {
+        // TODO: write logic here
+    }
+
+    public function times($argument1)
+    {
+        // TODO: write logic here
+    }
 }
